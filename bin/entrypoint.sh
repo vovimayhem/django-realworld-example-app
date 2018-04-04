7fbf3c1fe3ead3961641fb8bd2e982264d876865
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2018-04-04 12:57:44 -0500

    Fixed deployable entrypoint

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
 
-if [ "$1" = "web" ]
+# 1: Specify a default command, in case it wasn't issued:
+if [ -z "$1" ] || [ "$1" = "web" ]
 then
-  exec "python manage.py runserver 0.0.0.0:$PORT"
-else
-  exec "$@"
+  set -- python manage.py runserver 0.0.0.0:$PORT "$@"
 fi
+
+# 2: Execute the given or default command:
+exec "$@"
